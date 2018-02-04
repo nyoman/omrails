140f82a008b01f0ab74b64a2fee78c56d7a7d4b0
ing <inyoman_gurnitha@yahoo.com>
2018-02-04 13:18:38 +0700

    Completed assignment by adding contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   root 'pages#home'  # or get '/' => 'pages#home'
-  get '/about' => 'pages#about'
-
+  get '/about' 	 => 'pages#about'
+  get '/contact' => 'pages#contact'
 end
