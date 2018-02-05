4d2e94c6c66d9d6bb4ed63c9db99b838b52af194
ing <inyoman_gurnitha@yahoo.com>
2018-02-05 13:25:47 +0700

    32. Customize Devise Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
+  as :user do 
+  	get 'signin' 	 => 'devise/sessions#new'
+  	delete 'signout' => 'devise/sessions#destroy'
+  	get 'signup' 	 => 'devise/registrations#new'
+  end
+
   root 'pages#home'  # or get '/' => 'pages#home'
   get '/about' 	 => 'pages#about'
   get '/contact' => 'pages#contact'
+
 end
